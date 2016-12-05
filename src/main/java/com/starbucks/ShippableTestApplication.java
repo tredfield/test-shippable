9c4d947ee461b9e8ba68c106da5084c1e4c07e7a
Travis Redfield <travis.redfield@slalom.com>
2016-12-05 14:11:10 -0800

    initial commit

---
+++ src/main/java/com/starbucks/ShippableTestApplication.java
@@ -0,0 +1,12 @@
+package com.starbucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippableTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippableTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/starbucks/ShippableTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.starbucks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShippableTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
