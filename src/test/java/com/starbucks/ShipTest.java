1dbab42c91282acdcfbd2307f59fe017229cb118
Travis Redfield <travis.redfield@slalom.com>
2016-12-05 14:50:59 -0800

    add failing test

---
+++ src/test/java/com/starbucks/ShipTest.java
@@ -0,0 +1,13 @@
+package com.starbucks;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class ShipTest
+{
+  @Test
+  public void someTest()
+  {
+    Assert.assertTrue(false);
+  }
+}
